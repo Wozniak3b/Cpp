61f117d973e532dbe4a7b0e625894b1606e81398
Jakub Woźniak <kubawgli@onet.eu>
2024-12-28 15:08:30 +0100

    Create Debt hpp

---
+++ Debts.hpp
@@ -0,0 +1,29 @@
+#include <string>
+using std::string;
+
+class Debt {
+private:
+    float amount;
+    string description;
+    bool isPaid;
+
+public:
+    Debt(float amt, const std::string& desc, bool paid)
+        : amount(amt), description(desc), isPaid(paid) {}
+
+    float getAmount() const {
+        return amount;
+    }
+
+    string getDescription() const {
+        return description;
+    }
+
+    bool getIsPaid() const {
+        return isPaid;
+    }
+
+    void markAsPaid() {
+        isPaid = true;
+    }
+};
