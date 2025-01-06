1c632338b8e0d8e6fb304e2c4cda76e83cdc5de4
Jakub Woźniak <kubawgli@onet.eu>
2025-01-06 21:40:05 +0100

    Changing debts.hpp to use id and isPaid value in class

--- Debts.hpp
+++ Debts.hpp
@@ -4,13 +4,20 @@ using std::string;
 
 class Debt{
 private:
+    int m_id;
     string m_description;
     float m_amount;
     string m_receiver;
+    bool m_paid;
 public:
-    Debt(const string& desc, float amt, const string& receiver)
-        : m_description(desc), m_amount(std::round(amt*100.0)/100.0), m_receiver(receiver) {}
+    Debt(int id, const string& desc, float amt, const string& receiver, bool paid=false)
+        : m_id(id), m_description(desc), m_amount(std::round(amt*100.0)/100.0)
+        , m_receiver(receiver), m_paid(paid) {}
 
+    int getId() const {
+        return m_id;
+    }
+    
     float getAmount() const {
         return m_amount;
     }
@@ -23,16 +30,27 @@ public:
         return m_receiver;
     }
 
+    bool isPaid() const {
+        return m_isPaid;
+    }
+
+    void markAsPaid() {
+        m_isPaid = true;
+    }
+
     string serialize() const {
-        return m_description + "," + std::to_string(m_amount)+ "," + m_receiver;
+        return std::to_string(m_id) + "," + m_description + "," 
+        + std::to_string(m_amount)+ "," + m_receiver+ "," + (m_isPaid ? "1" : "0");
     }
 
     static Debt deserialize(const string& data) {
         std::istringstream ss(data);
-        std::string description, amountStr, receiver;
+        std::string idStr, description, amountStr, receiver, isPaidStr;
+        std::getline(ss, idStr, ',');
         std::getline(ss, description, ',');
         std::getline(ss, amountStr, ',');
         std::getline(ss, receiver, ',');
-        return Debt(description, std::stod(amountStr), receiver);
+        std::getline(ss, isPaidStr, ',');
+        return Debt(std::stoi(idStr), description, std::stod(amountStr), receiver, isPaidStr == "1");
     }
 };
